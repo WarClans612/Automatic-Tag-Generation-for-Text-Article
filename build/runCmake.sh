2987c0a008de58c1d81cfc43b6488a61fc96cc2b
Wilbert <wilbert.phen@gmail.com>
2020-06-15 22:41:00 +0800

    Adding CMakeLists.txt and script

---
+++ build/runCmake.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cmake -DCMAKE_PREFIX_PATH="${HOME}/Course/Automatic-Tag-Generation-for-Text-Article/libtorch" ..
+#    -DCUDNN_INCLUDE_PATH="/usr/local/cuda/include" \
+#    -DCUDNN_LIBRARY_PATH="/usr/local/cuda/lib64" \
+#    ..
