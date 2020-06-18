988448fd069c43d6c73316d4326e879ba216d7d4
Wilbert <wilbert.phen@gmail.com>
2020-06-18 17:34:45 +0800

    Update cmake command script

--- build/runCmake.sh
+++ build/runCmake.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmake -DCMAKE_PREFIX_PATH="${HOME}/Course/Automatic-Tag-Generation-for-Text-Article/libtorch" ..
+cmake -DCMAKE_PREFIX_PATH="${HOME}/Course/Automatic-Tag-Generation-for-Text-Article/libtorch;${HOME}/Course/Automatic-Tag-Generation-for-Text-Article/pybind11" ..
 #    -DCUDNN_INCLUDE_PATH="/usr/local/cuda/include" \
 #    -DCUDNN_LIBRARY_PATH="/usr/local/cuda/lib64" \
 #    ..
