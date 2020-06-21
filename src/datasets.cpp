b4c3f505fd475a3cda56f933eb9b29ee7bc6bd8b
Wilbert <wilbert.phen@gmail.com>
2020-06-21 16:44:58 +0800

    Modifying datasets.cpp file into header and cpp

---
+++ src/datasets.cpp
@@ -0,0 +1,14 @@
+// Developer: Wilbert (wilbert.phen@gmail.com)
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "datasets.h"
+
+class Data {
+public:
+
+private:
+
+};
